997450da96f7e47dad53815457c782d49ebcb92a
Jayden Keh <zhaoxikeh@gmail.com>
2023-02-20 18:59:32 +0800

    edited Header with menu and search bar and added CSS

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "../../styles/header.css";
+
+const navLinks = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/cars",
+    display: "Cars",
+  },
+  {
+    path: "/blogs",
+    display: "Blog",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
 
 const Header = () => {
   return (
@@ -34,6 +57,92 @@ const Header = () => {
               </div>
             </Col>
           </Row>
+        </Container>
+      </div>
+      <div className="header_middle">
+        <Container>
+          <Row>
+            <Col lg="4" md="3" sm="4">
+              <div className="logo">
+                <h1>
+                  <Link to="/home" className="d-flex align-items-center gap-2">
+                    <i className="ri-roadster-line"></i>
+                    <span>
+                      Rent Car
+                      <br />
+                      Service
+                    </span>
+                  </Link>
+                </h1>
+              </div>
+            </Col>
+            <Col lg="3" md="3" sm="4">
+              <div className="header_location d-flex align-items-center gap-2">
+                <span>
+                  <i className="ri-earth-line"></i>
+                </span>
+                <div className="header_location-content">
+                  <h4>West</h4>
+                  <h6>Bukit Batok</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg="3" md="3" sm="4">
+              <div className="header_location d-flex align-items-center gap-2">
+                <span>
+                  <i className="ri-time-line"></i>
+                </span>
+                <div className="header_location-content">
+                  <h4>Sunday to Friday</h4>
+                  <h6>10am - 7pm</h6>
+                </div>
+              </div>
+            </Col>
+            <Col
+              lg="2"
+              md="3"
+              sm="0"
+              className="d-flex align-items-center justify-content-end"
+            >
+              <button className="header_btn btn">
+                <Link to="/contact">
+                  <i className="ri-phone-line"></i>Request a call
+                </Link>
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="main_navbar">
+        <Container>
+          <div className="navigation_wrapper d-flex align-items-center justify-content-between">
+            <span className="mobile_menu">
+              <i className="ri-menu-line"></i>
+            </span>
+            <div className="navigation">
+              <div className="menu">
+                {navLinks.map((item, index) => (
+                  <NavLink
+                    to={item.path}
+                    className={(navClass) =>
+                      navClass.isActive ? "nav_active nav_item" : "nav_item"
+                    }
+                    key={index}
+                  >
+                    {item.display}
+                  </NavLink>
+                ))}
+              </div>
+            </div>
+            <div className="nav_right">
+              <div className="search_box">
+                <input type="text" placeholder="Search" />
+                <span>
+                  <i className="ri-search-line"></i>
+                </span>
+              </div>
+            </div>
+          </div>
         </Container>
       </div>
     </header>
