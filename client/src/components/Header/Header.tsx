3e07acde0882e2f6cd4205ac091a8c2674338422
Jayden Keh <zhaoxikeh@gmail.com>
2023-02-20 18:09:31 +0800

    added CSS to Header component

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
+import "../../styles/header.css";
 
 const Header = () => {
   return (
@@ -17,11 +18,17 @@ const Header = () => {
               </div>
             </Col>
             <Col lg="6" md="6" sm="6">
-              <div className="header_top_right">
-                <Link to="#">
+              <div
+                className="header_top_right
+                d-flex
+                align-items-center
+                justify-content-end
+                gap-3"
+              >
+                <Link to="#" className="d-flex align-items-center gap-1">
                   <i className="ri-login-box-line"></i>Login
                 </Link>
-                <Link to="#">
+                <Link to="#" className="d-flex align-items-center gap-1">
                   <i className="ri-user-3-line"></i>Register
                 </Link>
               </div>
