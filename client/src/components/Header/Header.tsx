db82be723ba285447cb5e3874e12d0d8a2281b09
Jayden Keh <zhaoxikeh@gmail.com>
2023-02-20 17:57:31 +0800

    setup Header component

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="header_top">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6">
+              <div className="header_top_left">
+                <span>Need Help?</span>
+                <span className="header_top_help">
+                  <i className="ri-phone-fill"></i> +65 6666 8888
+                </span>
+              </div>
+            </Col>
+            <Col lg="6" md="6" sm="6">
+              <div className="header_top_right">
+                <Link to="#">
+                  <i className="ri-login-box-line"></i>Login
+                </Link>
+                <Link to="#">
+                  <i className="ri-user-3-line"></i>Register
+                </Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
