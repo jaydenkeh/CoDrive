0ff04aa5fcdc19f65d0ca75f90b7a5b63a98db21
Jayden Keh <zhaoxikeh@gmail.com>
2023-02-20 19:02:01 +0800

    setup Contact page boiler template

--- client/src/routers/Routers.tsx
+++ client/src/routers/Routers.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
@@ -7,6 +6,7 @@ import CarListing from "../pages/CarListing";
 import Blog from "../pages/Blog";
 import BlogDetails from "../pages/BlogDetails";
 import NotFound from "../pages/NotFound";
+import Contact from "../pages/Contact";
 
 const Routers = () => {
   return (
@@ -18,6 +18,7 @@ const Routers = () => {
       <Route path="/cars/:slug" element={<CarDetails />} />
       <Route path="/blogs" element={<Blog />} />
       <Route path="/blogs/:slug" element={<BlogDetails />} />
+      <Route path="/contact" element={<Contact />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
