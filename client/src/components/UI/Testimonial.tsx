8607e0d7814f7126969f41ed12f10d83e5d0cc5c
Jayden Keh <zhaoxikeh@gmail.com>
2023-02-22 11:07:45 +0800

    added CSS to Testimonial section

--- client/src/components/UI/Testimonial.tsx
+++ client/src/components/UI/Testimonial.tsx
@@ -3,6 +3,7 @@ import ava01 from "../../assets/all-images/ava-1.jpg";
 import ava02 from "../../assets/all-images/ava-2.jpg";
 import ava03 from "../../assets/all-images/ava-3.jpg";
 import ava04 from "../../assets/all-images/ava-4.jpg";
+import "../../styles/testimonial.css";
 
 const Testimonial = () => {
   const settings = {
