2bf129e63ac2c6f84c95c653c92b36d06a1c26d6
Jayden Keh <zhaoxikeh@gmail.com>
2023-02-23 13:03:12 +0800

    created CarListing page

--- client/src/pages/CarListing.tsx
+++ client/src/pages/CarListing.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
+import CarItem from "../components/UI/CarItem";
+import carData from "../assets/data/carData";
 
 const CarListing = () => {
-  return <div>CarListing</div>;
+  return (
+    <Helmet title="Cars">
+      <CommonSection title="Car Listing" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <div className="d-flex align-items-center gap-3 mb-5">
+                <span className="d-flex align-items-center gap-2">
+                  <i className="ri-sort-asc">Sort By</i>
+                </span>
+                <select>
+                  <option>Select</option>
+                  <option value="Low">Low to High</option>
+                  <option value="high">High to Low</option>
+                </select>
+              </div>
+            </Col>
+            {carData.map((item) => (
+              <CarItem item={item} key={item.id} />
+            ))}
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default CarListing;
