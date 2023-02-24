671e020a584f6a71380e0e951d948f0d9b2c825d
Jayden Keh <zhaoxikeh@gmail.com>
2023-02-24 13:56:08 +0800

    added CSS to PaymentMethod component

--- client/src/components/UI/PaymentMethod.tsx
+++ client/src/components/UI/PaymentMethod.tsx
@@ -1,5 +1,6 @@
 import masterCard from "../../assets/all-images/master-card.jpg";
 import paypal from "../../assets/all-images/paypal.jpg";
+import "../../styles/payment-method.css";
 
 const PaymentMethod = () => {
   return (
